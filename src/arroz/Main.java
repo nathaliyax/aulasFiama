979bfe65fc6ca2e3fd9ce1a7c306cdf4a548cf37
nathaliyax <nathalialimas@hotmail.com>
2025-08-21 10:40:19 -0300

    feat: first commit

---
+++ src/arroz/Main.java
@@ -0,0 +1,11 @@
+package arroz;
+
+import static arroz.imc.getImc;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(getImc(80,1.60));
+
+    }
+}
+
---
+++ src/arroz/imc.java
@@ -0,0 +1,6 @@
+package arroz;
+public class imc {
+    public static double getImc(double peso, double altura ){
+        return(peso/(altura*altura));
+    }
+}
