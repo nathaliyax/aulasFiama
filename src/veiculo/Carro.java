c9b00d09750d2f4c72f14205112e65c0d46dd79f
nathaliyax <nathalialimas@hotmail.com>
2025-08-21 11:08:12 -0300

    feat: exc dia 21

---
+++ src/veiculo/Carro.java
@@ -0,0 +1,7 @@
+package veiculo;
+
+public class Carro {
+    public static void veiculo(){
+        System.out.println("Carro esta iniciando...");
+    }
+}
---
+++ src/veiculo/Main.java
@@ -0,0 +1,9 @@
+package veiculo;
+
+import static veiculo.Carro.veiculo;
+
+public class Main {
+    public static void main(String[] args) {
+        veiculo();
+    }
+}
